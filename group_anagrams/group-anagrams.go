5020293d0b505364006328b39ebc1fc30ba98581
Sterling Deng <sterlingdeng@gmail.com>
2019-11-10 12:18:34 -0500

    anagram sadness

---
+++ group_anagrams/group-anagrams.go
@@ -0,0 +1,30 @@
+package group_anagrams
+
+/*
+https://leetcode.com/problems/group-anagrams/
+medium
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+
+ */
+
+
+func GroupAnagrams(s []string) [][]string {
+	var output [][]string
+
+
+	return output
+}
