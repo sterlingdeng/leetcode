672285c327d14e76cbea581f35295eedd97388a0
Sterling Deng <sterlingdeng@gmail.com>
2019-11-21 18:39:55 -0800

    add heap data structure

---
+++ data_structures/heap.go
@@ -0,0 +1,87 @@
+package data_structures
+
+import (
+	"errors"
+	"math"
+)
+
+var ErrHeapEmpty = errors.New("heap empty")
+
+type Heap struct {
+	heap       []int
+	comparator func(a, b int) bool
+}
+
+func NewHeap(comparator func(a, b int) bool) Heap {
+	return Heap{
+		comparator: comparator,
+	}
+}
+
+func (h *Heap) Peek() (int, error) {
+	if len(h.heap) == 0 {
+		return -1, ErrHeapEmpty
+	}
+	return h.heap[0], nil
+}
+
+func (h *Heap) Add(n int) {
+	h.heap = append(h.heap, n)
+	h.siftUp()
+}
+
+func (h *Heap) Remove() (int, error) {
+	if len(h.heap) == 0 {
+		return -1, ErrHeapEmpty
+	}
+	removed := h.heap[0]
+	swap(h.heap, 0, len(h.heap)-1)
+	h.heap = h.heap[:len(h.heap)-1]
+	h.siftDown(0)
+	return removed, nil
+}
+
+func (h *Heap) siftUp() {
+	currIdx := len(h.heap) - 1
+	parentIdx := getParent(currIdx)
+	for parentIdx >= 0 && h.comparator(h.heap[currIdx], h.heap[parentIdx]) {
+		swap(h.heap, currIdx, parentIdx)
+		currIdx = parentIdx
+		parentIdx = getParent(currIdx)
+	}
+}
+
+func (h *Heap) siftDown(idx int) {
+	heapSize := len(h.heap)
+	element := idx
+	leftIdx := getLeftChild(idx)
+	rightIdx := getRightChild(idx)
+	if leftIdx < heapSize && h.comparator(h.heap[leftIdx], h.heap[element]) {
+		element = leftIdx
+	}
+	if rightIdx < heapSize && h.comparator(h.heap[rightIdx], h.heap[element]) {
+		element = rightIdx
+	}
+	if element == idx {
+		return
+	}
+	swap(h.heap, element, idx)
+	h.siftDown(element)
+}
+
+func getParent(idx int) int {
+	parentIdx := (float64(idx) - 1.0) / 2
+	return int(math.Floor(parentIdx))
+}
+
+func getLeftChild(idx int) int {
+	return idx*2 + 1
+}
+
+func getRightChild(idx int) int {
+	return idx*2 + 2
+}
+
+func swap(arr []int, a, b int) {
+	arr[a], arr[b] = arr[b], arr[a]
+}
---
+++ data_structures/heap_test.go
@@ -0,0 +1,90 @@
+package data_structures
+
+import (
+	"reflect"
+	"testing"
+)
+
+var MinHeapFn = func(a, b int) bool { return a < b }
+
+var MaxHeapFn = func(a, b int) bool { return a > b }
+var MinHeap *Heap
+
+//var MaxHeap Heap
+
+func init() {
+	h := NewHeap(MinHeapFn)
+	MinHeap = &h
+	//MaxHeap = NewHeap(MAX_HEAP_FN)
+}
+
+func TestHeap_Add(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		heap *Heap
+		args args
+		want []int
+	}{
+		{
+			name: "add to min heap",
+			heap: MinHeap,
+			args: args{5},
+			want: []int{5},
+		},
+		{
+			name: "add to min heap",
+			heap: MinHeap,
+			args: args{2},
+			want: []int{2, 5},
+		},
+		{
+			name: "add to min heap",
+			heap: MinHeap,
+			args: args{10},
+			want: []int{2, 5, 10},
+		},
+		{
+			name: "add to min heap",
+			heap: MinHeap,
+			args: args{1},
+			want: []int{1, 2, 10, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.heap.Add(tt.args.n)
+			if !reflect.DeepEqual(tt.want, tt.heap.heap) {
+				t.Errorf("Heap should be %v but got %v", tt.want, tt.heap.heap)
+			}
+		})
+	}
+}
+
+func TestHeap_Remove(t *testing.T) {
+	h := NewHeap(MaxHeapFn)
+	maxHeap := &h
+	maxHeap.Add(1)
+	if peeked, _ := maxHeap.Peek(); peeked != 1 {
+		t.Errorf("Max heap peek should be 1, but got %d", peeked)
+	}
+
+	maxHeap.Add(10)
+	if peeked, _ := maxHeap.Peek(); peeked != 10 {
+		t.Errorf("Max heap peek should be 10, but got %d", peeked)
+	}
+	maxHeap.Add(4)
+	if peeked, _ := maxHeap.Peek(); peeked != 10 {
+		t.Errorf("Max heap peek should be 10, but got %d", peeked)
+	}
+
+	if removed, _ := maxHeap.Remove(); removed != 10 {
+		t.Errorf("Max heap removed should be 10, but got %d", removed)
+	}
+
+	if peeked, _ := maxHeap.Peek(); peeked != 4 {
+		t.Errorf("Max heap peek should be 4, but got %d", peeked)
+	}
+}
